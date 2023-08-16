fa03715aab9137aefb60b1b1cd663db10b1f0215
orange-soba <t.sato.terusa6k@gmail.com>
2023-08-17 02:52:11 +0900

    BASIC認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      #発展カリキュラムLESSON5ユーザー認証の導入を確認
+    end
+  end
 end
